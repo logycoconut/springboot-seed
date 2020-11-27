ee6b2035ff783611672ee8aea82e573230cb1522
logycoconut <1425795337@qq.com>
2020-11-27 22:43:30 +0800

    init commit

---
+++ src/test/java/com/logycoco/seed/SpringbootSeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logycoco.seed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
